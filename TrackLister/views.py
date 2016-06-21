2020fb58441a4f91e704796b90079452b9620310
Gaurev Chand Katoch <gcoomecoo@gmail.com>
2016-06-21 19:07:32 +0530

    Trying out templates

--- TrackLister/views.py
+++ TrackLister/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.serializers import serialize
-import json
+from django.template import loader
 from models import Tracks, Genres
 
 # Create your views here.
@@ -9,8 +9,12 @@ from models import Tracks, Genres
 
 def tracks(request):
 
-    track_list = serialize("json", Tracks.objects.all())
-    return HttpResponse(track_list)
+    # track_list = serialize("json", Tracks.objects.all())
+    track_list = Tracks.objects.all()
+    template = loader.get_template('templates/tracks.html')
+    context = {'tracks_list': track_list, }
+
+    return HttpResponse(template.render(context, request))
 
 
 def genres(request):
