b7a0869284d4f9bb29e201a31fb47dcf44291c80
Gaurev Chand Katoch <gcoomecoo@gmail.com>
2016-06-21 18:53:01 +0530

    Album Field Added

--- TrackLister/models.py
+++ TrackLister/models.py
@@ -9,6 +9,7 @@ class Tracks(models.Model):
     track_name = models.CharField(max_length=100, blank=False)
     genre = models.CharField(max_length=50, null=True, blank=True)
     artist = models.CharField(max_length=100, blank=True)
+    ablum = models.CharField(max_length=100, blank=True)
     release_year = models.PositiveIntegerField(null=True, blank=True)
     rating = models.PositiveIntegerField(null=True, blank=True)
 
@@ -22,8 +23,3 @@ class Tracks(models.Model):
 
 class Genres(models.Model):
     genre = models.CharField(max_length=50, blank=False)
-
-    def clean(self):
-        from django.core.exceptions import ValidationError
-        if self.genre == '':
-            raise ValidationError('Genre name cannot be empty')
