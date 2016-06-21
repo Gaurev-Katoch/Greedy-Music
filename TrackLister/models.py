452dce604aa2077d3daabc6bb603b6cb32f1ca01
Gaurev Chand Katoch <gcoomecoo@gmail.com>
2016-06-21 18:29:32 +0530

    Migration files added

--- TrackLister/models.py
+++ TrackLister/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from django.db import models
+from datetime import date
 
 # Create your models here.
 
@@ -8,13 +9,15 @@ class Tracks(models.Model):
     track_name = models.CharField(max_length=100, blank=False)
     genre = models.CharField(max_length=50, null=True, blank=True)
     artist = models.CharField(max_length=100, blank=True)
-    release_year = models.IntegerField(null=True, blank=True)
+    release_year = models.PositiveIntegerField(null=True, blank=True)
     rating = models.PositiveIntegerField(null=True, blank=True)
 
     def clean(self):
         from django.core.exceptions import ValidationError
-        if self.track_name == '':
-            raise ValidationError('Track name cannot be empty')
+        if self.rating > 5:
+            raise ValidationError('Rating should be between 0 & 5')
+        elif self.release_year > date.today().year:
+            raise ValidationError('Release year cannot be after current year')
 
 
 class Genres(models.Model):
